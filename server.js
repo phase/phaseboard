d70e0932179fde87680a9b1428c214fe4c232a32
phase <ph4sesaber@gmail.com>
2016-03-25 23:25:55 -0700

    Convert main page to Stylus and load it with options

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
-  , template = require('jade').compileFile(__dirname + '/src/templates/default.jade')
+  , template = require('jade').compileFile(__dirname + '/src/templates/outline.jade')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
   try {
-    var html = template({ title: 'Home' })
+    var html = template({
+        //todo: put in options.json
+        title: 'Home',
+        theme: 'yule',
+        meta: {
+            description: "This is some meta text",
+            keywords: "such board much wow"
+        },
+        infobox: {
+            title: "This is a title",
+            text: "todo: markdown parse this"
+        },
+        
+        //should be retrieve from somewhere
+        user: {
+            name: "phase",
+            id: 0,
+            rank: "Owner"
+        }
+    })
     res.send(html)
   } catch (e) {
     next(e)
